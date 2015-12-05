f23dc05a8d3838fa7d845042d0654ed08a20d3a4
Manfred Touron <m@42.am>
2015-12-05 23:32:58 +0100

    Initial commit

---
+++ manfred-touron.go
@@ -0,0 +1,28 @@
+package moul
+
+import "github.com/moul/manfred-touron"
+
+type ManfredTouron struct {
+	Firstname string
+	Lastname  string
+	Website   string
+	GitHub    string
+	Twitter   string
+	Location  string
+	Headline  string
+	Emoji     string
+	Groups    []string
+}
+
+func GetManfredTouron() ManfredTouron {
+	return ManfredTouron{
+		Firstname: manfredtouron.Firstname,
+		Lastname:  manfredtouron.Lastname,
+		Website:   manfredtouron.Website,
+		GitHub:    manfredtouron.GitHub,
+		Twitter:   manfredtouron.Twitter,
+		Location:  manfredtouron.Location,
+		Headline:  manfredtouron.HeadLine,
+		Emoji:     manfredtouron.Emoji,
+	}
+}
