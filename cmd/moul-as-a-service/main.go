a78043b93a61837755371c38338a4985b9bd5469
Manfred Touron <m@42.am>
2015-12-05 23:47:53 +0100

    Listing available actions

--- cmd/moul-as-a-service/main.go
+++ cmd/moul-as-a-service/main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"fmt"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/moul/as-a-service"
 )
 
+type Action func([]string) (interface{}, error)
+type Actions map[string]Action
+
+func GetManfredTouron(args []string) (interface{}, error) {
+	return moul.GetManfredTouron(), nil
+}
+
 func main() {
-	mt := moul.GetManfredTouron()
-	fmt.Println(mt)
+	actions := map[string]Action{
+		"manfred-touron": GetManfredTouron,
+	}
+
+	action := "manfred-touron"
+	ret, err := actions[action](nil)
+	if err != nil {
+		logrus.Fatalf("Failed to execute %q: %v", action, err)
+	}
+	fmt.Printf("%v\n", ret)
 }
