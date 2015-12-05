e9d9669dea897935bdb74a748c10f1ca53dd1c0e
Manfred Touron <m@42.am>
2015-12-05 23:58:59 +0100

    Using codegangsta/cli for parsing

--- cmd/moul-as-a-service/main.go
+++ cmd/moul-as-a-service/main.go
@@ -3,25 +3,46 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
 	"github.com/moul/as-a-service"
 )
 
 type Action func([]string) (interface{}, error)
-type Actions map[string]Action
 
 func GetManfredTouron(args []string) (interface{}, error) {
 	return moul.GetManfredTouron(), nil
 }
 
+var Actions map[string]Action
+
+func init() {
+	Actions = make(map[string]Action)
+	Actions["manfred-touron"] = GetManfredTouron
+}
+
 func main() {
-	actions := map[string]Action{
-		"manfred-touron": GetManfredTouron,
+	app := cli.NewApp()
+	app.Name = "moul-as-a-service"
+	app.Usage = "moul, but as a service"
+	app.Commands = []cli.Command{}
+
+	for action := range Actions {
+		command := cli.Command{
+			Name:   action,
+			Action: CliActionCallback,
+		}
+		app.Commands = append(app.Commands, command)
 	}
 
-	action := "manfred-touron"
-	ret, err := actions[action](nil)
+	app.Run(os.Args)
+}
+
+func CliActionCallback(c *cli.Context) {
+	action := c.Command.Name
+	ret, err := Actions[action](c.Args())
 	if err != nil {
 		logrus.Fatalf("Failed to execute %q: %v", action, err)
 	}
