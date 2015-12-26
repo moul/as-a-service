b6bb6d72e9d5149ef799f80e4a38ca269609913f
Manfred Touron <m@42.am>
2015-12-26 09:10:45 +0100

    Add appspot support

--- appspot/app.go
+++ appspot/app.go
@@ -1,14 +1,36 @@
 package moulasaservice
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/moul/as-a-service"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	for name := range moul.Actions() {
+		http.HandleFunc(fmt.Sprintf("/%s", name), actionHandler)
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world !")
+func actionHandler(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimLeft(r.URL.Path, "/")
+	if fn, found := moul.Actions()[path]; found {
+		args := []string{}
+		ret, err := fn(args)
+
+		if err != nil {
+			http.Error(w, fmt.Sprintf("service error: %v\n", err), 500)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			enc := json.NewEncoder(w)
+			if err := enc.Encode(&ret); err != nil {
+				http.Error(w, fmt.Sprintf("json encode error: %v\n", err), 500)
+			}
+		}
+	} else {
+		http.NotFound(w, r)
+	}
 }
