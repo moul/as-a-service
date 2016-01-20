d79242ac4d668bbb58a3f5bd546352685608aac4
Manfred Touron <m@42.am>
2016-01-20 23:00:09 +0100

    Add github repos

--- github.go
+++ github.go
@@ -2,6 +2,7 @@ package moul
 
 import (
 	"github.com/SlyMarbo/rss"
+	"github.com/google/go-github/github"
 	"github.com/patrickmn/go-cache"
 )
 
@@ -9,6 +10,7 @@ var feed *rss.Feed
 
 func init() {
 	RegisterAction("github-activity", GetGithubActivityAction)
+	RegisterAction("github-repos", GetGithubReposAction)
 }
 
 func GetGithubActivityAction(args []string) (interface{}, error) {
@@ -23,6 +25,18 @@ func GetGithubActivityAction(args []string) (interface{}, error) {
 	return activity, nil
 }
 
+func GetGithubReposAction(args []string) (interface{}, error) {
+	if repos, found := moulCache.Get("github-repos"); found {
+		return repos, nil
+	}
+	repos, err := GetGithubRepos()
+	if err != nil {
+		return nil, err
+	}
+	moulCache.Set("github-repos", repos, cache.DefaultExpiration)
+	return repos, nil
+}
+
 func GetGithubActivity() (*rss.Feed, error) {
 	var err error
 	if feed == nil {
@@ -31,4 +45,11 @@ func GetGithubActivity() (*rss.Feed, error) {
 		err = feed.Update()
 	}
 	return feed, err
+}
+
+func GetGithubRepos() (interface{}, error) {
+	client := github.NewClient(nil)
+	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+	repos, _, err := client.Repositories.List("moul", opt)
+	return repos, err
 }
