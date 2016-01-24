186212f928154b07b8cb6a9975fb75d0e0dc9fad
Manfred Touron <m@42.am>
2016-01-24 21:31:20 +0100

    Using new manfred-touron module

--- manfred-touron.go
+++ manfred-touron.go
@@ -6,31 +6,6 @@ func init() {
 	RegisterAction("manfred-touron", GetManfredTouronAction)
 }
 
-type ManfredTouron struct {
-	Firstname string
-	Lastname  string
-	Website   string
-	GitHub    string
-	Twitter   string
-	Location  string
-	Headline  string
-	Emoji     string
-	Groups    []string
-}
-
 func GetManfredTouronAction(args []string) (interface{}, error) {
-	return GetManfredTouron(), nil
-}
-
-func GetManfredTouron() ManfredTouron {
-	return ManfredTouron{
-		Firstname: manfredtouron.Firstname,
-		Lastname:  manfredtouron.Lastname,
-		Website:   manfredtouron.Website,
-		GitHub:    manfredtouron.GitHub,
-		Twitter:   manfredtouron.Twitter,
-		Location:  manfredtouron.Location,
-		Headline:  manfredtouron.Headline,
-		Emoji:     manfredtouron.Emoji,
-	}
+	return manfredtouron.Manfred, nil
 }
